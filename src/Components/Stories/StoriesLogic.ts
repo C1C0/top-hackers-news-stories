ff5df306fc2df9327578be78d939038ecae1f013
C1C0 <kpskola1002@gmail.com>
2021-10-07 20:36:19 +0200

    Documentation for stories

--- src/Components/Stories/StoriesLogic.ts
+++ src/Components/Stories/StoriesLogic.ts
@@ -1,16 +1,20 @@
 import { Story } from "../../Shared/interfaces";
 
 /**
- *
+ * @return sorted stories
  */
-export const getTopStories = async () => {
+export const getTopStories = async (): Promise<Story[]> => {
   const allTopStories = await fetchTopStories();
   const topStories = await getRandomTopStories(10, allTopStories);
 
   return orderStoriesByScore(topStories);
 };
 
-const fetchTopStories = async () => {
+/**
+ * Fetch raw data (ids of top stories) from API
+ * @returns raw ids of top stories
+ */
+const fetchTopStories = async (): Promise<number[]> => {
   const other = await fetch(`${getHNAPI()}/topstories.json`, {
     mode: "cors",
   });
@@ -18,30 +22,43 @@ const fetchTopStories = async () => {
   return await other.json();
 };
 
+/**
+ * From provided stories choses random x stories id, fetches their objects from API
+ *
+ * @param {number} xSelectedStories number of selected stories
+ * @param {number[]} topStories ids of topStories
+ * @returns fetched stories array from API
+ */
 const getRandomTopStories = async (
   xSelectedStories: number,
   topStories: number[]
-) => {
+): Promise<Story[]> => {
   let selectedStories: Story[] = [];
   let lengthOfTopStories: number = topStories.length;
 
   for (let i = 0; i < xSelectedStories; i++) {
     const randomIndex = Math.round(Math.random() * lengthOfTopStories + 1);
     const story = await fetchStoryData(topStories[randomIndex]);
-
-    console.log(story);
-
     selectedStories.push(story);
   }
 
   return selectedStories;
 };
 
-const fetchStoryData = async (id: number) => {
+/**
+ *
+ * @param id ID of story
+ * @returns Story object
+ */
+const fetchStoryData = async (id: number): Promise<Story> => {
   const story = await fetch(`${getHNAPI()}/item/${id}.json`);
   return await story.json();
 };
 
+/**
+ * Checks if REACT_APP_HACKER_NEWS_API provided and returns it's value
+ * @returns REACT_APP_HACKER_NEWS_API link
+ */
 const getHNAPI = (): string => {
   if (
     process.env.REACT_APP_HACKER_NEWS_API &&
@@ -54,6 +71,13 @@ const getHNAPI = (): string => {
   }
 };
 
+/**
+ * Orders stories by it's score
+ * @param stories
+ * @returns
+ */
 const orderStoriesByScore = (stories: Story[]): Story[] => {
   return stories.sort((a: Story, b: Story): number => {
+    return a.score - b.score;
+  });
 };
