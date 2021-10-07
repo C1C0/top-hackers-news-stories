51a18a59f3e1ee17a25dedec800d06147f4b9a2c
C1C0 <kpskola1002@gmail.com>
2021-10-07 20:02:45 +0200

    Add spinner

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import { Container } from "react-bootstrap";
 import { css, jsx } from "@emotion/react";
 import { Header } from "./Components/Header/Header";
 import { Stories } from "./Components/Stories/Stories";
+import LoadingSpinner from "./Components/LoadingSpinner/LoadingSpinner";
 
 function App() { 
   return (
     <Container className="App">
       <Header title={process.env.REACT_APP_APP_TITLE || ""} mantra={process.env.REACT_APP_APP_MANTRA}/>
-      <Stories />
+      {/* <Stories /> */}
+      <LoadingSpinner />
     </Container>
   );
 }
